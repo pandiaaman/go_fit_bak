bf6a22bc8f6405521495b28903614fee0729c939
Aman Pandia <pandiaaman16@gmail.com>
2023-08-13 22:21:20 +0530

    initial checkout for go fit bak, happy beginnings

---
+++ src/main/java/com/pandiaaman/gofitbak/GoFitBakApplication.java
@@ -0,0 +1,13 @@
+package com.pandiaaman.gofitbak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoFitBakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoFitBakApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pandiaaman/gofitbak/GoFitBakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pandiaaman.gofitbak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoFitBakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
